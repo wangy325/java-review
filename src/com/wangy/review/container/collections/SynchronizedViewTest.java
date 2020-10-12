b9a71212a4737443ea3386d112fbada62e759718
wangy325_win <wangy325@qq.com>
2020-10-12 23:58:53 +0800

    线程安全的集合/视图也需要同步

--- src/com/wangy/review/container/collections/SynchronizedViewTest.java
+++ src/com/wangy/review/container/collections/SynchronizedViewTest.java
@@ -6,11 +6,58 @@ import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * undone demo
+ * 线程安全的集合中“线程安全”的含义是多线程对集合元素的操作是同步的<br>
+ * <p>
+ * 但是
+ * <pre>
+ *     private void task(List<Integer> list) {
+ *         for (int i = 0; i < 50; i++) {
+ *             cal_count.getAndIncrement();
+ *             synchronized (this) {
+ *                 list.set(0, list.get(0) + i);
+ *             }
+ *         }
+ *     }
+ * </pre>
+ * 方法中对<code>list</code>的更改还是需要使用同步，不仅对{@link ArrayList}如此，
+ * 对{@link Collections#synchronizedList(List)}和{@link Vector}都是如此<br>
+ * 如果不使用同步，那么在
+ * <pre>
+ *     private void test(List<Integer> list) throws InterruptedException {
+ *         int count = 0;
+ *         System.out.println("程序10s后自动退出");
+ *         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+ *         service.schedule(() -> System.exit(0), 10, TimeUnit.SECONDS);
+ *         for (; ; ) {
+ *             count++;
+ *             unsafeIncrAndReplace(list);
+ *             if (list.get(0) != 2450) {
+ *                 System.out.println(list.get(0) + ": " + cal_count + ", 第" + count + "次出现讹误");
+ *                 break;
+ *             }
+ *         }
+ *     }
+ * </pre>
+ * 方法中的for循环始终会出现讹误<br>
+ * 对于线程不安全的{@link ArrayList}来说，这是容易理解的，因为多个线程同时对集合内容进行更改会引发安全问题<br>
+ * 但对于线程安全的同步视图和{@link Vector}来说，这应该如何理解？<br>
+ * 问题就出在
+ * <pre>
+ *     list.set(0, list.get(0) + i);
+ * </pre>
+ * 上，上述逻辑等价于
+ * <PRE>
+ *     int y = list.get(0) + i;
+ *     // 可以被中断
+ *     list.set(0,y);
+ * </PRE>
+ * 这样会导致集合的值“滞后”更新，因此测试下来，发现出现讹误的情况都是最后的值&lt;2450
+ * </p>
  *
  * @author wangy
  * @version 1.0
@@ -19,15 +66,21 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class SynchronizedViewTest {
 
 
-    /** 同步集合 */
+    /**
+     * 同步集合-- 对集合元素的操作是同步的，使用synchronized关键字获取this的锁
+     */
     private static List<Integer> synchronizedList = Collections.synchronizedList(new ArrayList<Integer>() {{
         add(0);
     }});
-    /** 非同步集合 */
-    private static List<Integer> list = new ArrayList<Integer>() {{
+    /**
+     * 非同步集合
+     */
+    private static List<Integer> unsafe_list = new ArrayList<Integer>() {{
         add(0);
     }};
-    /** 同步集合 */
+    /**
+     * 同步集合
+     */
     private static Vector<Integer> vector = new Vector<Integer>() {{
         add(0);
     }};
@@ -37,49 +90,26 @@ public class SynchronizedViewTest {
     public static void main(String[] args) throws InterruptedException {
 
         SynchronizedViewTest svt = new SynchronizedViewTest();
-
-        svt.testSynchronizedList();
-        /*while (true){
-//            if (svt.testExecutorsShutdown() != 2450) break;
-            System.out.println(svt.testExecutorsShutdown());
-            Thread.sleep(2000);
-        }*/
-
-//        unsafeIncrAndReplace(vector);
-//        System.out.println(vector.get(0));
-//        unsafeIncrAndReplace(list);
-//        System.out.println(list.get(0));
+//        svt.test(synchronizedList);
+//        svt.test(vector);
+        svt.test(unsafe_list);
     }
 
-    void testSynchronizedList() throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
-            unsafeIncrAndReplace(synchronizedList);
-
-           /* if (synchronizedList.get(0) != 2450){
-                System.out.println(synchronizedList.get(0) + ": " + count);
+    private void test(List<Integer> list) throws InterruptedException {
+        int count = 0;
+        System.out.println("程序10s后自动退出");
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.schedule(() -> System.exit(0), 10, TimeUnit.SECONDS);
+        for (; ; ) {
+            count++;
+            unsafeIncrAndReplace(list);
+            if (list.get(0) != 2450) {
+                // 这里获取到的值一定是任务执行完毕之后的最新值
+                // 如果任务的操作不是同步的，那么此循环一定会break
+                System.out.println(list.get(0) + ": " + cal_count + ", 第" + count + "次出现讹误");
                 break;
-            }*/
-            TimeUnit.SECONDS.sleep(1);
-            System.out.println(synchronizedList.get(0) + ": " + cal_count);
+            }
         }
-    }
-
-    int testExecutorsShutdown() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
-        AtomicInteger i = new AtomicInteger(0);
-        executorService.execute(() -> {
-            for (int j = 0; j < 50; j++) {
-                i.set(i.intValue() + j);
-            }
-        });
-        executorService.execute(() -> {
-            for (int j = 0; j < 50; j++) {
-                i.set(i.intValue() + j);
-            }
-        });
-        executorService.shutdown();
-//        executorService.awaitTermination(2, TimeUnit.SECONDS);
-        return i.intValue();
     }
 
     /**
@@ -87,29 +117,30 @@ public class SynchronizedViewTest {
      *
      * @param list
      */
-    void unsafeIncrAndReplace(List<Integer> list) throws InterruptedException {
+    private void unsafeIncrAndReplace(List<Integer> list) throws InterruptedException {
         list.set(0, 0);
         cal_count.set(0);
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
-
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
         // thread-1
-        executorService.execute(() -> {
-            for (int i = 0; i < 50; i++) {
-                cal_count.getAndIncrement();
-                list.set(0, list.get(0) + i);
-            }
-        });
+        executorService.execute(() -> task(list));
 
         // thread-2
-        executorService.execute(() -> {
-            for (int i = 0; i < 50; i++) {
-                cal_count.getAndIncrement();
-                list.set(0, list.get(0) + i);
-            }
-        });
+        executorService.execute(() -> task(list));
 
         executorService.shutdown();
-//        executorService.awaitTermination(2, TimeUnit.SECONDS);
+        // 阻塞当前线程直到所有任务执行完毕/或超时
+        executorService.awaitTermination(2, TimeUnit.SECONDS);
+//        System.out.println(executorService.isTerminated()); // true
+    }
+
+    private void task(List<Integer> list) {
+        for (int i = 0; i < 50; i++) {
+            cal_count.getAndIncrement();
+            // 需要同步
+            synchronized (this) {
+                list.set(0, list.get(0) + i);
+            }
+        }
     }
 }
