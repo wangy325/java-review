99d28b047d840aee7c8c31d8e1f2919621afe622
wangy325 <wangy325@qq.com>
2020-11-13 23:19:32 +0800

    Test ScheduledThreadPoolExecutor

--- src/com/wangy/review/concurrency/component/TestScheduledPoolExecutor.java
+++ src/com/wangy/review/concurrency/component/TestScheduledPoolExecutor.java
@@ -13,31 +13,68 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TestScheduledPoolExecutor {
 
     private AtomicInteger sequence = new AtomicInteger(0);
+    private ScheduledThreadPoolExecutor service;
+
+    public TestScheduledPoolExecutor(int corePoolSize) {
+        this.service = new ScheduledThreadPoolExecutor(corePoolSize);
+    }
 
     private void s() {
-        System.out.println(Thread.currentThread() + " " + sequence.getAndIncrement());
+        System.out.println(Thread.currentThread() + " " + sequence.incrementAndGet());
     }
 
-    private void i() {
+    private void c() {
+        System.out.println(Thread.currentThread() + " c running");
         while (true) {
-            // do nothing
+            // never finish loop unless interrupted
+            if (Thread.interrupted()) {
+                break;
+            }
         }
+        System.out.println(Thread.currentThread() + "c interrupted");
     }
 
-    void test1() {
-        ScheduledThreadPoolExecutor service = new ScheduledThreadPoolExecutor(1);
-
+    @SneakyThrows
+    void basicTest() {
 //        service.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
-        service.schedule(this::s, 2, TimeUnit.HOURS);
-        service.schedule(this::s, 1, TimeUnit.SECONDS);
+        service.schedule(this::s, 2, TimeUnit.SECONDS);
+        service.schedule(this::c, 1, TimeUnit.SECONDS);
 
+        service.shutdown();
+//        service.shutdownNow();
+        TimeUnit.SECONDS.sleep(5);
+        System.exit(0);
+    }
+
+    @SneakyThrows
+    void howManyThreads() {
+
+        // 如果使用默认策略取消一个任务，那么非核心线程就不会被销毁
+        /*
+        service.schedule(this::s, 2, TimeUnit.SECONDS);
+        BlockingQueue<Runnable> queue = service.getQueue();
+        FutureTask<Void> task = (FutureTask<Void>) queue.peek();
+        task.cancel(false);
+         */
+
+        for (; ; ) {
+            ScheduledFuture<?> schedule = service.schedule(this::s, 0, TimeUnit.MILLISECONDS);
+//            TimeUnit.MILLISECONDS.sleep(5);
+            for (; ; ) {
+                if (schedule.isDone())
+                    break;
+            }
+            if (sequence.get() >= 10) {
+                schedule.cancel(false);
+                break;
+            }
+        }
+        System.out.println("largest pool size: " + service.getLargestPoolSize());
         service.shutdown();
     }
 
     @SneakyThrows
     void cancelSchedule() {
-        ScheduledThreadPoolExecutor service = new ScheduledThreadPoolExecutor(1);
-
         service.setRemoveOnCancelPolicy(true);
         // task to cancelled
         service.schedule(this::s, 10, TimeUnit.SECONDS);
@@ -59,8 +96,6 @@ public class TestScheduledPoolExecutor {
 
     @SneakyThrows
     void shutdownPolicy() {
-        ScheduledThreadPoolExecutor service = new ScheduledThreadPoolExecutor(2);
-
         // 如果任务在shutdown()之后仍在delay，那么将值设置为false可以取消任务的执行
         // 其默认值为true
         service.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
@@ -79,12 +114,22 @@ public class TestScheduledPoolExecutor {
         System.out.println(service.isTerminated());
     }
 
-    public static void main(String[] args) {
-        TestScheduledPoolExecutor ts = new TestScheduledPoolExecutor();
+    @SneakyThrows
+    void shutdownNow() {
+        service.schedule(this::c, 0, TimeUnit.MILLISECONDS);
 
-        ts.test1();
+        TimeUnit.MILLISECONDS.sleep(1000);
+//        service.shutdown();
+        service.shutdownNow();
+    }
+
+    public static void main(String[] args) {
+        TestScheduledPoolExecutor ts = new TestScheduledPoolExecutor(0);
+//        ts.basicTest();
+//        ts.howManyThreads();
 //        ts.cancelSchedule();
-//        ts.shutdownPolicy();
+        ts.shutdownPolicy();
+//        ts.shutdownNow();s
     }
 
 }
