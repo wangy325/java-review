c21d4c46088247cc56980bce603fccd89dda3b10
wangy <wangyong@dongxinsoft.cn>
2021-01-27 19:16:42 +0800

    ignore .iml file

---
+++ src/main/java/com/wangy/review/CompareChain.java
@@ -0,0 +1,21 @@
+package com.wangy.review;
+
+/**
+ * @author wangy
+ * @date 01.26/0026 16:00
+ */
+public class CompareChain {
+
+
+    class Person {
+        String lastname;
+        String firstname;
+        long zipcode;
+
+
+
+    }
+
+
+
+}
