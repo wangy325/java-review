c7f6dbd4150f03a91f33a4cfc96a06d0c87f3288
wangy <wangyong@dongxinsoft.cn>
2021-01-27 11:46:02 +0800

    use springboot skeleton.

---
+++ src/main/java/com/wangy/ReviewApplication.java
@@ -0,0 +1,16 @@
+package com.wangy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangy
+ * @date 01.27/0027 11:38
+ */
+@SpringBootApplication
+public class ReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewApplication.class, args);
+    }
+}
