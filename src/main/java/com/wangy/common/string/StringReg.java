70f5469b534467eaf62dbb7b6f66f5194c5ed5f5
wangy <wangyong@dongxinsoft.cn>
2021-03-04 15:51:38 +0800

    regex groups

--- src/main/java/com/wangy/common/string/StringReg.java
+++ src/main/java/com/wangy/common/string/StringReg.java
@@ -2,6 +2,7 @@ package com.wangy.common.string;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -14,6 +15,9 @@ public class StringReg {
     static String pattern2 = "ab.cd.ef";
     static String pattern3 = "java.util.String";
     static String pattern4 = "ok";
+    static  String pattern5 = "/profile/upload/2020/09/15/9d6ca80b4030ec3589568e376f9a1507.png";
+
+    static String pattern6 = "hello@mary";
 
     static String pattern12(String reg) {
         boolean b1 = Pattern.matches(reg, pattern1);
@@ -29,8 +33,18 @@ public class StringReg {
         return Pattern.compile(reg).matcher(pattern1).replaceFirst(pattern1.substring(0,1).toUpperCase());
     }
 
+    static String getSuffix(String reg){
+        Matcher matcher = Pattern.compile(reg).matcher(pattern5);
+        if (matcher.find()){
+            return matcher.group(1);
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         log.info(pattern12("^([a-z]+\\.)+[a-z]+$"));
         log.info(replaceFirst("\\w"));
+        log.info(getSuffix("(\\.\\w{3,4})"));
+//        log.info(getSuffix("@(\\S+)"));
     }
 }
