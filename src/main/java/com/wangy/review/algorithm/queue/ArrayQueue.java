072308d14edc39cdcd5db28ee8cae7303a06a208
wangy325 <wangy325@qq.com>
2021-06-30 23:40:04 +0800

    [数据结构与算法之美]：队列 数组实现 数组满的条件判断，加入图形解释

--- src/main/java/com/wangy/review/algorithm/queue/ArrayQueue.java
+++ src/main/java/com/wangy/review/algorithm/queue/ArrayQueue.java
@@ -16,24 +16,87 @@ public class ArrayQueue {
     private final int cap;
     // index of head element
     private int head;
-    // index of tail element
+    /*
+        关于tail指针，一般是将tail指向最新的可以插入元素的index，即tail是一个空槽（slot）：
+        ===================================================
+         data[0]                                   data[7]
+         |    |    |  5  |  6  |  9  |  16  |      |     |
+                    head                      tail
+        ===================================================
+         初始化时，head = tail = 0，此后每插入一个元素，tail后移一位。
+
+         当tail = cap且head=0，即认为队列"已满"：
+         ===================================================
+         data[0]                                   data[7]
+         |  14  |  8  |  5  |  6  |  9  |  16  |  87  |  67  |
+           head                                                 tail
+         ===================================================
+
+         当head = tail时，元素都已出队，此时队列为空：
+         ===================================================
+         data[0]                                   data[7]
+         |    |    |     |     |     |      |      |     |
+                                       head
+                                       tail
+         ===================================================
+
+         特殊地，当tail = cap且head != 0时，数组明明有空间，元素却无法入队，这时候就需要将head-tail指针重新整理了：
+         =======================================================
+         data[0]                                   data[7]
+         |    |    |     |     |     |  18  |  34  |  45  |
+                                       head                 tail
+         ======================================================================
+           |  |            data[0]                                     data[7]
+            \  \---\¯\     | 18  |  34  |  45  |     |     |     |      |     |
+             \  \--/_/      head                 tail
+                         ======================================================
+      */
+    // index of tail
     private int tail;
 
     public ArrayQueue(int cap) {
         this.data = new int[cap];
         this.cap = cap;
-        head = tail = -1;
+        head = tail = 0;
+    }
+
+
+    int getHead() {
+        if (isEmpty()) return -1;
+        return data[head];
+    }
+
+    int getTail() {
+        if (isEmpty()) return -1;
+        return data[tail - 1];
+    }
+
+    boolean isFull() {
+        return tail == cap && head == 0;
+    }
+
+    boolean isEmpty() {
+        return head == tail;
+    }
+
+    boolean reposition() {
+        return tail == cap && head != 0;
     }
 
     // 入队
     int enQueue(int e) {
         // full
         if (isFull()) throw new ArrayStoreException("队列满了");
-        // empty
-        if (isEmpty()) {
-            head++;
+        // 整理元素
+        if (reposition()) {
+            for (int i = head; i < tail; i++) {
+                data[i - head] = data[i];
+                data[i] = 0;
+            }
+            tail -= head;
+            head = 0;
         }
-        data[++tail] = e;
+        data[tail++] = e;
         return e;
     }
 
@@ -44,25 +107,6 @@ public class ArrayQueue {
         return data[head++];
     }
 
-    int getHead() {
-        if (isEmpty()) return -1;
-        return data[head];
-    }
-
-    int getTail() {
-        if (isEmpty()) return -1;
-        return data[tail];
-    }
-
-    boolean isFull() {
-        return tail == cap - 1;
-    }
-
-    boolean isEmpty() {
-        // tail = -1
-        // head = cap
-        return tail == -1 || head == cap;
-    }
 
     // 打印数组/队列
     void printAll() {
@@ -70,8 +114,8 @@ public class ArrayQueue {
         System.out.print("队列：[");
         if (!isEmpty()) {
             //
-            for (int i = head; i <=tail; i++) {
-                if (i == tail)
+            for (int i = head; i < tail; i++) {
+                if (i == tail - 1)
                     System.out.print(data[i]);
                 else
                     System.out.print(data[i] + ", ");
@@ -83,11 +127,13 @@ public class ArrayQueue {
     public static void main(String[] args) {
         ArrayQueue aq = new ArrayQueue(3);
         aq.enQueue(1);
+        System.out.println(aq.deQueue());
         aq.enQueue(2);
         aq.enQueue(3);
+        aq.enQueue(4);
+
         System.out.println(aq.deQueue());
         System.out.println(aq.deQueue());
-//        System.out.println(aq.deQueue());
 //        System.out.println(aq.deQueue());
 
         System.out.printf("队列头尾索引和值：%s: %s, %s: %s%n", aq.head, aq.getHead(), aq.tail, aq.getTail());
--- src/main/java/com/wangy/review/algorithm/queue/ArrayQueueUnbounded.java
+++ src/main/java/com/wangy/review/algorithm/queue/ArrayQueueUnbounded.java
@@ -16,45 +16,74 @@ public class ArrayQueueUnbounded {
     private int cap;
     // index of head element
     private int head;
-    // index of tail element
+    /*
+       关于tail指针，一般是将tail指向最新的可以插入元素的index，即tail是一个空槽（slot）：
+       ===================================================
+        data[0]                                   data[7]
+        |    |    |  5  |  6  |  9  |  16  |      |     |
+                   head                      tail
+       ===================================================
+        初始化时，head = tail = 0，此后每插入一个元素，tail后移一位。
+
+        当head = tail时，认为队列已空，因为tail是一个空槽：
+        ===================================================
+        data[0]                                   data[7]
+        |    |    |     |     |     |     |      |     |
+                                            tail
+                                            head
+        ===================================================
+
+        当tail = cap时，即认为队列"已满"：
+        ========================================================
+        data[0]                                   data[7]
+        |    |    |  5  |  6  |  9  |  16  |  87  |  56  |
+                   head                                    tail
+        ========================================================
+
+        有一个特殊的情况，即 head = tail = cap-1 时，此时队列是空的，也是满的，
+        不过其不影响队列后续的入队：
+        ===================================================
+        data[0]                                   data[7]
+        |    |    |     |     |     |     |      |     |
+                                                   tail
+                                                   head
+        ===================================================
+     */
+    // index of tail element + 1
     private int tail;
 
     public ArrayQueueUnbounded() {
         // initialization capacity: 4
         this.data = new int[4];
         this.cap = 4;
-        this.head = this.tail = -1;
+        this.head = this.tail = 0;
     }
 
     boolean isFull() {
-        return tail == cap - 1;
+        // 当tail指向最后一个空槽时，即认为队列已满
+        return tail == cap ;
     }
 
     boolean isEmpty() {
-        // tail = -1
-        // head = tail +1
-        return tail == -1 || head == tail + 1;
+        // 无论指针在何处，只要2个指针"相遇"，即认为队列为空
+        return head == tail;
     }
 
     // 入队 平均时间复杂度O(1)
     int enQueue(int e) {
-        // full, grow, and reposition
         // 若队列满了，那么增加容量，并且将队列中的元素进行重排
         // |x|x|1|2| => |1|2|-|-|-|-|
         if (isFull()) {
             cap += cap >> 1;
             int[] newData = new int[cap];
-            for (int i = head; i <= tail; i++) {
+            for (int i = head; i < tail; i++) {
                 newData[i - head] = data[i];
             }
             data = newData;
             tail -= head;
             head = 0;
         }
-        if (isEmpty()) {
-            head++;
-        }
-        data[++tail] = e;
+        data[tail++] = e;
         return e;
     }
 
@@ -69,8 +98,8 @@ public class ArrayQueueUnbounded {
         System.out.println("数组：" + Arrays.toString(data));
         System.out.print("队列：[");
         if (!isEmpty()) {
-            for (int i = head; i <= tail; i++) {
-                if (i == tail)
+            for (int i = head; i < tail; i++) {
+                if (i == tail - 1)
                     System.out.print(data[i]);
                 else
                     System.out.print(data[i] + ", ");
@@ -82,10 +111,14 @@ public class ArrayQueueUnbounded {
     public static void main(String[] args) {
         ArrayQueueUnbounded aqu = new ArrayQueueUnbounded();
         aqu.enQueue(1);
+        aqu.deQueue();
         aqu.enQueue(2);
         aqu.enQueue(3);
         aqu.enQueue(4);
         aqu.deQueue();
+        aqu.deQueue();
+        aqu.deQueue();
+//        aqu.deQueue();
         aqu.enQueue(5);
         aqu.printAll();
     }
